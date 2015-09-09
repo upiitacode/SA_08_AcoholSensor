7896411ac17e9b1d57c66e4e7c36895508124503
Manuel Limones <limonesu.me@gmail.com>
2015-09-09 09:54:54 -0500

    configure led

--- source/main.c
+++ source/main.c
@@ -1,4 +1,17 @@
 #include "stm32f30x.h"                  // Device header
+
+void led_init(void);
+
 int main(){
+	led_init();
 	return 0;
 }
+
+void led_init(void){
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	GPIOB->MODER &=~(GPIO_MODER_MODER13);
+	GPIOB->MODER |=GPIO_MODER_MODER13_0;//output
+	GPIOB->ODR |= GPIO_ODR_13;
+}
+
+
