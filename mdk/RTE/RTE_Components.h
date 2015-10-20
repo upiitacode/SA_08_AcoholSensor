2efd1c2a9e038b343b66de5c3319385756e42c29
Manuel Limones <limonesu.me@gmail.com>
2015-10-20 03:40:40 -0500

    Add RTOS configuration

--- mdk/RTE/RTE_Components.h
+++ mdk/RTE/RTE_Components.h
@@ -10,5 +10,7 @@
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
 
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
 
 #endif /* RTE_COMPONENTS_H */
--- source/main.cc
+++ source/main.cc
@@ -1,11 +1,13 @@
 #include "retarget_stm32f3.h"                  // Device header
+#include <stm32f3xx.h>
 
 int main(){
 	//System configuration
 	Serial_stream* serial = new SerialUSART2(9600);
-
+	SystemCoreClockUpdate();
 	//User application
 	serial->printf("Hello World\n");
+	serial->printf("ProcessorSpeed %d\n",SystemCoreClock);
 	while(1){
 	}
 }
